e81c930ee2e0bc09ecc319d735bf666206bc164c
HassanKhan123 <hk2602809@gmail.com>
2021-05-31 22:53:22 +0500

    signup and login button added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
+import { Link } from 'react-router-dom';
 
 import fetchCurrentUser from '../queries/CurrentUser';
 
@@ -8,12 +9,35 @@ const Header = () => {
 
   const { data, loading, error } = useQuery(FETCH_CURRENT_USER);
 
-  console.log('DATA==========', data);
+  const renderButtons = () => {
+    if (loading) return <div></div>;
 
-  if (loading) {
-    return <h3>Loading.....</h3>;
-  }
-  return <div>Header</div>;
+    if (data.user) {
+      return <div>Logout</div>;
+    }
+
+    return (
+      <div>
+        <li>
+          <Link to='/signup'>Signup</Link>
+        </li>
+        <li>
+          <Link to='/login'>Login</Link>
+        </li>
+      </div>
+    );
+  };
+
+  return (
+    <nav>
+      <div className='nav-wrapper'>
+        <Link to='/' className='brand-logo left'>
+          Home
+        </Link>
+        <ul className='right'>{renderButtons()}</ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
